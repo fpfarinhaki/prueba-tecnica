e214be88e625ed5a2fa8a85e843d772efb7c58c5
Felipe P. Farinhaki <fpossenti@corelogic.com>
2024-09-11 19:42:43 +0200

    Remove spring rest data support to avoid exposure of unwanted methods

--- src/main/java/com/decskill/prueba/dataaccess/repository/PriceJpaRepository.java
+++ src/main/java/com/decskill/prueba/dataaccess/repository/PriceJpaRepository.java
@@ -3,10 +3,8 @@ package com.decskill.prueba.dataaccess.repository;
 import com.decskill.prueba.domain.entity.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RepositoryRestResource(path = "prices")
 public interface PriceJpaRepository extends JpaRepository<Price, Integer>, JpaSpecificationExecutor<Price> {
 }
